29ccaef5b237b3e6959e74fa17f346837abf7a0f
jalcayaga <javier.alcayaga@subred.cl>
2023-08-15 04:55:52 -0400

    reparando boton de get started

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -58,10 +58,9 @@ const Hero = () => {
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient"></div>
         <div className="absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white-gradient"></div>
         <div className="absolute z-[0] w-[50%] h-[50%] bottom-20 blue__gradient"></div>
-
-        <div className={`ss:hidden ${styles.flexCenter}`}>
-          <GetStarted />
-        </div>
+      </div>
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+        <GetStarted />
       </div>
     </section>
   );
